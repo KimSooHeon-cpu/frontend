fe4b6294be571ad1664e5b7553b1cdd5bc5b220d
KimSooHeon-cpu <rlatn1219@naver.com>
2025-11-10 12:49:17 +0900

    Change link to use API endpoint for board navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -185,7 +185,7 @@ const Navbar: React.FC = () => {
                         <Button
                           key={board.boardId}
                           component={Link}
-                          to={`/board/${board.boardNum}`}
+                          to={`/api/board/${board.boardNum}`}
                           fullWidth
                           sx={{
                             justifyContent: "flex-start",
