7918c47392d9af4931b0a015aba9561efe11248f
KimSooHeon-cpu <rlatn1219@naver.com>
2025-11-10 11:51:10 +0900

    api제거

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -185,7 +185,7 @@ const Navbar: React.FC = () => {
                         <Button
                           key={board.boardId}
                           component={Link}
-                          to={`/api/board/${board.boardNum}`}
+                          to={`/board/${board.boardNum}`}
                           fullWidth
                           sx={{
                             justifyContent: "flex-start",
