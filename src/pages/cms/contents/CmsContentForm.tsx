38f62d0a9af46590667e661c3ddbcc00f489f280
javateacher <javateacher@naver.com>
2025-11-12 17:08:19 +0900

    'cors_update'

--- src/pages/cms/contents/CmsContentForm.tsx
+++ src/pages/cms/contents/CmsContentForm.tsx
@@ -135,8 +135,7 @@ export default function CmsContentForm() {
                 if (form.contentFilePath) {
                     params.append("contentFilePath", form.contentFilePath);
                 }
-                console.log("수정 직전 : http://16.176.33.172:8181/api/cms/contents/update : ", contentId)
-                //await api.put(`/api/cms/contents/${contentId}`, params, config); // PUT 요청으로 수정
+                console.log("수정 직전 : http://16.176.33.172:8181/api/cms/contents : ", contentId)                
                 await api.put(`http://16.176.33.172:8181/api/cms/contents/${contentId}`, params, config);  // [251112] 수정 주소 다이렉트로 붙이기                 
                 alert("콘텐츠가 수정되었습니다."); // 사용자에게 성공 알림
             } else { // 신규 등록 모드일 경우
